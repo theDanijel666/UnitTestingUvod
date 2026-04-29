5a83263a09207a50f0f5f5188721c9cae9cc99b3
Danijel Pobi <121439317+theDanijel666@users.noreply.github.com>
2026-04-29 18:23:50 +0200

    Dodan test controllera

---
+++ WebMvc.UnitTest/HomeControllerTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shared.Models.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WebMvc.Controllers;
+
+namespace WebMvc.UnitTest
+{
+    public class HomeControllerTests : WebMvcSetup
+    {
+        [Fact]
+        public async Task HomeController_ReturnsViewWithData()
+        {
+            var controller = new HomeController(null, vehicleService);
+
+            var res = controller.Index();
+
+            var viewResult = Assert.IsType<ViewResult>(res);
+            Assert.True(viewResult.ViewName == null || viewResult.ViewName=="Index");
+            var model=Assert.IsAssignableFrom<List<VehicleViewModel>>(viewResult.ViewData.Model);
+            Assert.Equal(16,model.Count);
+        }
+    }
+}
